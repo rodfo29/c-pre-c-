8fbe7d3bd35affae442bfc968c14846b3cfc09f9
rodfo29 <martinpinto.2820@gmail.com>
2025-02-03 05:36:12 -0400

    Contraseña aleatoria python

--- ComparingStrings.c
+++ ComparingStrings.c
@@ -25,9 +25,17 @@ int comp(char*st,char*st2){
 
 int main(){
 
-    char nombre[]="martin";
+    // char nombre[]="martin";
     char nombre2[]="martin";
     
-    printf("%d ",comp(nombre,nombre2));
+    // printf("%d ",comp(nombre,nombre2));
+
+    char nombre[7];
+    printf("Ingresar nombre: ");
+    scanf("%7s",nombre); // Para cadenas es mejor usar fgets a comparación de scanf. Amenos que en el scanf pongamos de tamaño 1 menos al tamaño del buffer, a diferencia de 
+    // fgets(nombre,7,stdin); // fgets que si le podemos poner el mismo tamaño del buffer y este automáticamente nos leerá 1 menos del tamaño establecido.
+    printf("\nNombre: %s\n",nombre);
+
+
 
 }
---
+++ randompassword.py
@@ -0,0 +1,20 @@
+import string
+import random
+
+# Esta es la representación de estas secuencias de caracteres.
+numbers=string.digits #'123456789'
+characters=string.ascii_lowercase # 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+mayusculas=string.ascii_uppercase # 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+
+
+#De esta manera podemos obtenes una contraseña que es producto de concatenar distintas secuencias
+# de caracteres.
+newpassword=random.choices(numbers+characters+caracteresEspeciales+mayusculas,k=20)
+
+array=[1,2,3,4,5,6]
+
+
+
+
+# Hacemos que la contraseña generada tenga buen formato
+print(f"Este es el número aleatorio : {"".join(newpassword)}")
